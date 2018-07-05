0602befa05dc620850963ae677f5873f4edbd593
Felipe Rodrigues <rodrigues882013>
2018-07-05 09:35:34 +0000

    -- removing database file

--- photogallery/gallery/forms.py
+++ photogallery/gallery/forms.py
@@ -1,9 +1,9 @@
-from django.forms import ModelForm, FileField, ClearableFileInput, CharField, TextInput
+from django.forms import ModelForm, FileField, ClearableFileInput, CharField, TextInput, ImageField
 from gallery.models import Image
 
 
 class ImageForm(ModelForm):
-    image = FileField(widget=ClearableFileInput(attrs={'multiple': True}))
+    image = ImageField(widget=ClearableFileInput(attrs={'multiple': True}))
     file_name = CharField(widget=TextInput(attrs={'class': 'form-control', 'placeholder': 'Photo Title'}))
 
     class Meta:
